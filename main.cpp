63f917361da9c8c5bf4705bc4978368cd5890bd5
Javier Gómez González <javigg011205@gmail.com>
2025-11-01 23:07:38 +0100

    DevIl implemented

--- main.cpp
+++ main.cpp
@@ -1,7 +1,60 @@
+#define IL_NO_WCHAR_T
 #include <glad/glad.h>
 #include <SDL3/SDL.h>
 #include "shader_s.h"
-#include "stb_image.h"
+#include <IL/il.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <fstream>
+#include <vector>
+
+#include <string>
+
+static GLuint LoadTextureDevIL(const char* path, bool genMipmaps = true) {
+    // 1) Leer el archivo en memoria
+    std::ifstream f(path, std::ios::binary);
+    if (!f) {
+        SDL_Log("No se pudo abrir %s", path);
+        return 0;
+    }
+    std::vector<unsigned char> buf((std::istreambuf_iterator<char>(f)),
+                                    std::istreambuf_iterator<char>());
+
+    // 2) Crear imagen y cargar desde memoria con DevIL (independiente de wchar/char)
+    ILuint img = 0;
+    ilGenImages(1, &img);
+    ilBindImage(img);
+
+    if (!ilLoadL(IL_TYPE_UNKNOWN, buf.data(), (ILuint)buf.size())) {
+        ILenum err = ilGetError();
+        SDL_Log("DevIL: fallo al cargar '%s' desde memoria (err=%d)", path, (int)err);
+        ilDeleteImages(1, &img);
+        return 0;
+    }
+
+    // 3) Normalizar a RGBA8 y subir a OpenGL
+    ilConvertImage(IL_RGBA, IL_UNSIGNED_BYTE);
+
+    const int w = ilGetInteger(IL_IMAGE_WIDTH);
+    const int h = ilGetInteger(IL_IMAGE_HEIGHT);
+    const void* pixels = ilGetData();
+
+    GLuint tex = 0;
+    glGenTextures(1, &tex);
+    glBindTexture(GL_TEXTURE_2D, tex);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, genMipmaps ? GL_LINEAR_MIPMAP_LINEAR : GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
+    if (genMipmaps) glGenerateMipmap(GL_TEXTURE_2D);
+
+    ilDeleteImages(1, &img);
+    return tex;
+}
+
 
 int main(int argc, char *args[])
 {
@@ -52,6 +105,10 @@ int main(int argc, char *args[])
         return -1;
     }
 
+    ilInit();
+    ilEnable(IL_ORIGIN_SET);
+    ilOriginFunc(IL_ORIGIN_LOWER_LEFT);
+
     // --- Set Viewport ---
     // Define the OpenGL rendering area
     glViewport(0, 0, 800, 600);
@@ -65,7 +122,7 @@ int main(int argc, char *args[])
     SDL_Log("OpenGL version (from GLAD): %d.%d", GLVersion.major, GLVersion.minor);
     SDL_Log("OpenGL profile (from GLAD): %s", GLAD_GL_VERSION_3_1 ? "Core 3.1+" : "Other");
 
-    Shader shader("4.2.texture.vs", "4.2.texture.fs");
+    Shader shader("5.1.transform.vs", "5.1.transform.fs");
 
 
     float vertices[] = {
@@ -103,46 +160,12 @@ int main(int argc, char *args[])
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-    stbi_set_flip_vertically_on_load(true); // igual que en LearnOpenGL 4.2
+    GLuint texture1 = LoadTextureDevIL("resources/textures/container.jpg");
+    if (!texture1) SDL_Log("No se pudo cargar container.jpg");
 
-    unsigned int texture1, texture2;
+    GLuint texture2 = LoadTextureDevIL("resources/textures/awesomeface.png");
+    if (!texture2) SDL_Log("No se pudo cargar awesomeface.png");
 
-    // texture1: container.jpg (RGB)
-    glGenTextures(1, &texture1);
-    glBindTexture(GL_TEXTURE_2D, texture1);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-    int w, h, n;
-    unsigned char* data = stbi_load("resources/textures/container.jpg", &w, &h, &n, 0);
-    if (data) {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, w, h, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-    } else {
-        SDL_Log("No se pudo cargar resources/textures/container.jpg");
-    }
-    stbi_image_free(data);
-
-    // texture2: awesomeface.png (RGBA)
-    glGenTextures(1, &texture2);
-    glBindTexture(GL_TEXTURE_2D, texture2);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-    data = stbi_load("resources/textures/awesomeface.png", &w, &h, &n, 0);
-    if (data) {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-    } else {
-        SDL_Log("No se pudo cargar resources/textures/awesomeface.png");
-    }
-    stbi_image_free(data);
-
-    // Asignar unidades a los samplers del shader (solo una vez)
     shader.use();
     shader.setInt("texture1", 0);
     shader.setInt("texture2", 1);
@@ -189,7 +212,14 @@ int main(int argc, char *args[])
         glActiveTexture(GL_TEXTURE1);
         glBindTexture(GL_TEXTURE_2D, texture2);
 
+        glm::mat4 transform = glm::mat4(1.0f);
+        transform = glm::translate(transform, glm::vec3(0.5f, -0.5f, 0.0f));
+        transform = glm::rotate(transform, (float)SDL_GetTicks() / 1000.0f, glm::vec3(0.0f, 0.0f, 1.0f));
+
         shader.use();
+        int transformLoc = glGetUniformLocation(shader.ID, "transform");
+        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
+
         glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
