8fdd5325ec07034485dc6a6bb6275c04c34cb26d
Javier Gómez González <javigg011205@gmail.com>
2025-11-01 21:12:10 +0100

    Texture of a box

--- main.cpp
+++ main.cpp
@@ -87,6 +87,9 @@ int main(int argc, char *args[])
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
@@ -95,6 +98,10 @@ int main(int argc, char *args[])
 
     glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
     glEnableVertexAttribArray(2);
+
+    glBindVertexArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
 
     GLuint tex;
     glGenTextures(1, &tex);
