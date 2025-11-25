cd822ec30e03b636771b7c3f9a7cf3ef8710395b
oscaralonsoo <alonsocamenforte@gmail.com>
2025-11-25 04:33:39 +0100

    Shader attribute mismatch causing distorted UVs

--- Assets/shaders/Shader.glsl
+++ Assets/shaders/Shader.glsl
@@ -1,8 +1,11 @@
 #[vertex]
 #version 330 core
 
-layout (location = 0) in vec3 aPos;
-layout (location = 1) in vec2 aTexCoord;
+layout (location = 0) in vec3 aPosition;
+layout (location = 1) in vec3 aNormals;
+layout (location = 2) in vec2 aTexCoords;
+layout (location = 3) in vec3 aTangent;
+layout (location = 4) in vec3 aBitangent;
 
 out vec2 TexCoord;
 
@@ -12,8 +15,8 @@ uniform mat4 projection;
 
 void main()
 {
-    gl_Position = projection * view * model * vec4(aPos, 1.0f);
-    TexCoord = aTexCoord;
+    gl_Position = projection * view * model * vec4(aPosition, 1.0);
+    TexCoord = aTexCoords;
 }
 
 #[fragment]
@@ -23,7 +26,6 @@ out vec4 FragColor;
 
 in vec2 TexCoord;
 
-// texture samplers
 uniform sampler2D texture1;
 uniform sampler2D texture2;
 
