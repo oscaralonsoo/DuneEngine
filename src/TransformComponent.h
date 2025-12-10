49a6afa127fd0eb35bae7e130e4dff58bb88cbe6
Toni Llovera <160241512+ToniLlovera@users.noreply.github.com>
2025-12-10 23:51:16 +0100

    Update TransformComponent.h

--- src/TransformComponent.h
+++ src/TransformComponent.h
@@ -24,6 +24,8 @@ public:
 
 private:
     void UpdateWorldMatrix();
+    glm::mat4 ComputeLocalMatrix() const;
+
     glm::vec3 position{0.0f, 0.0f, 0.0f};
     glm::vec3 rotation{0.0f, 0.0f, 0.0f};
     glm::vec3 scale{1.0f, 1.0f, 1.0f};
