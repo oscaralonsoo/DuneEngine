1de26f30f8f4729df6f45004dddb4bacf5531467
Toni Llovera <160241512+ToniLlovera@users.noreply.github.com>
2025-12-12 01:25:52 +0100

    Frame Buffer Impl / Scene & Game View

--- src/Framebuffer.h
+++ src/Framebuffer.h
@@ -10,4 +10,13 @@ public:
 
     void Bind();
     void Unbind();
+    void Resize(uint32_t width, uint32_t height);
+    uint32_t GetColorAttachment() const;
+    uint32_t GetWidth() const;
+    uint32_t GetHeight() const;
+
+private:
+    uint32_t m_Width, m_Height;
+    uint32_t m_RendererID;
+    uint32_t m_ColorAttachment, m_DepthAttachment;
 };
